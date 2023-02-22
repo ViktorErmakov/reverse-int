82283893f85bd93f22fa355cbbf8e58a9c51eb2f
Viktor Ermakov <79091602201@yandex.ru>
2023-02-22 23:48:16 +0300

    feart: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    n = Math.max(n, -n);
+    let arrStr = n.toString().split('');
+    let str = arrStr.reverse().toString();
+    let result = '';
+    for (const char of str) {
+        if(char === ','){
+            continue
+        }else{
+            result += char;
+        }
+    }
+    console.log(result);
+    return Number(result);
 }
+
+module.exports(-192);
